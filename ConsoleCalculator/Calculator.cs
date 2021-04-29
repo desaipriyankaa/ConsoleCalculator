6065d4b826b319bfe394164681947c1786e3acec
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-29 17:08:35 +0530

    global unhandled exception handeling (start w/o debugging)

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -6,7 +6,7 @@ namespace ConsoleCalculator
     {
         public int Calculate(int number1, int number2, string operation)
         {
-            throw new ArgumentNullException(nameof(number1));
+           // throw new ArgumentNullException(nameof(number1));
 
             string nonNullOperation = operation ?? throw new ArgumentNullException(nameof(operation)); 
 
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
+
+            AppDomain CurrentAppDomain = AppDomain.CurrentDomain;
+            CurrentAppDomain.UnhandledException += new UnhandledExceptionEventHandler(HandelException);
             WriteLine("Enter first number");
             int number1 = int.Parse(ReadLine());
 
@@ -20,7 +23,7 @@ namespace ConsoleCalculator
             var calculator = new Calculator();
             try
             {
-                int result = calculator.Calculate(number1, number2, null);
+                int result = calculator.Calculate(number1, number2, operation);
                 DisplayResult(result);
             }
             catch (ArgumentNullException ex) when (ex.ParamName=="operation")
@@ -48,6 +51,11 @@ namespace ConsoleCalculator
             
             WriteLine("\nPress enter to exit.");
             ReadLine();
+        }
+
+        private static void HandelException(object sender, UnhandledExceptionEventArgs e)
+        {
+            WriteLine($"Sorry there was a problem and we need to close . Details : {e.ExceptionObject}");
         }
 
         private static void DisplayResult(int result)
